33d99957bd7c95675e029bbe62da11bc418ea927
geekyed <eddweston@me.com>
2022-07-01 14:38:17 +0100

    Where we got to.

--- src/file.ts
+++ src/file.ts
@@ -1,3 +1,12 @@
 export const foo = () => 'hello Ashar and Rui and Ed'
 
-export const calculate = (numbers: number[]): string => ''
+export const calculate = (numbers: number[]): number => {
+    if (numbers.length !== 5) throw new Error('Hahahahahaha')
+
+    numbers.forEach(number => {
+        if (number < 1 || number > 6) {
+            throw new Error('Hahahahahaha')
+        }
+    })
+    return 150
+}
